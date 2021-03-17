79946d85126bfc7473f6b048b87753381a1cb564
Sameer Doshi <sadoshi@microsoft.com>
2021-03-17 14:48:27 -0500

    #1485 Actually preregister user

--- src/App.js
+++ src/App.js
@@ -1,8 +1,84 @@
 import React, { Component } from 'react';
 import { Button, Icon } from 'semantic-ui-react'
 import nh4h from './apis/nh4h';
+import RegForm from "./components/regform"
 import * as Msal from "msal";
+class User {
+  static APIURL='/users/regemail';
+  static ROLE="UserRole";
+  static REGEMAIL="UserRegEmail";
+  static TEAMSEMAIL="UserMSTeamsEmail";
+  static DISPLAYNAME="UserDisplayName";
+  static TIMECOMMITMENT="UserTimeCommitment";
+  static ACTIVE="Active";
+  static SKILLS="tblUserSkillMatch";
+  static OPTOUT="UserOptOut";
+  static SKILL="UserSkill";
+ 
+ userid;
+ email;
+ name;
+ role;
+ mySkills;
+ attemptedPreReg;
+ active;
+ constructor(){
+  this.userid=false;
+  this.role="Preregistrant";
+  this.active=false;
+  this.attemptedPreReg=false;
+ }
 
+ /**
+  * sets user id, preregisters user if needed
+  */
+ getUserID = () => {
+  let body = {};
+  body[User.REGEMAIL] = this.email ;
+  nh4h.post(User.APIURL, body)
+    .then((response) => {
+      if(!response.returnError){
+        
+        this.userid= response.data.userId;
+        this.mySkills= response.data.mySkills;
+       
+      }else{
+        console.log("Not preregistered");
+        this.preRegister();
+      }
+    });
+}
+/**
+ * This is here for documentation purposes and won't be called b/c getting the userID auto-reregisters user
+ */
+isPreregistered(){
+  return this.userid?true:false;
+}
+
+preRegister=()=>{
+  //circuit breaker to prevent infinite loops
+  if(this.attemptedPreReg) {return;}
+  let body={};
+    body[User.REGEMAIL]=this.email;
+    body[User.TEAMSEMAIL]=this.email;
+    body[User.ROLE]=this.role;
+    body[User.ACTIVE]=this.active;
+    nh4h.post(User.APIURL, body)
+    .then((response)=>{
+      if(response.returnError){
+        this.attemptedPreReg=true;
+      }else{
+        this.getUserID();
+      }
+    })
+    .catch(err => {
+      this.attemptedPreReg=true;
+      console.error(err);
+    });
+    ;
+}
+
+}
 
 class App extends Component {
   constructor(props){
@@ -17,9 +93,8 @@ class App extends Component {
     let msalI = new Msal.UserAgentApplication(msalConfig);
     this.state = {
       msalInstance: msalI,
-      email: "",
-      userid: null,
-      visible: true
+      user: new User(),
+      loggedin: false,
     }
   }
 
@@ -30,7 +105,7 @@ class App extends Component {
   componentDidMount() {
 
     if (this.state.msalInstance.getAccount()) {
-      console.log("Signted in");
+      
       this.processSignIn();
     
 
@@ -38,27 +113,18 @@ class App extends Component {
   }
   processSignIn =() =>{
     let id = this.state.msalInstance.getAccount();
+    
+    this.state.user.email=id.userName;
+    this.state.user.name=id.name;
+    this.state.user.getUserID();        
     this.setState({
-      loggedin: true,
-      email: id.userName,
-      username: id.name
-    }, () => {
-      this.getUserID();        
+      loggedin: true
     });
   }
-  getUserID = () => {
-    let body = { UserMSTeamsEmail: this.state.email };
-    nh4h.post('/users/msemail', body)
-      .then((response) => {
-        if(!response.returnError){
-        this.setState({ 
-          userObject:response.data,
-          userid: response.data.userId,
-          mySkills: response.data.mySkills
-         });
-        }
-      });
-  }
+  
+  
+
+ 
 
   signin = ()=>{
     let loginRequest = {
@@ -72,9 +138,7 @@ class App extends Component {
         // handle error
       });
   } 
-  handleDismiss = () => {
-    this.setState({ visible: false })
-  }
+  
  
   render() { 
     
@@ -84,15 +148,18 @@ class App extends Component {
 
     return(
       <div>
+        <div >
         {!this.state.loggedin?
               <Button onClick={this.signin} color='linkedin'>
               <Icon name='linkedin' /> Sign In with Microsoft
             </Button>
              
-              :
+              :""}
+        </div>
         
-        <div className="ui segment">
-         Welcome {this.state.email} You are Logged
+        <div>
+         Welcome {this.state.user.email} 
+         <RegForm/>
         </div>
   }
       </div>
---
+++ src/components/regform.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class RegForm extends React.Component {
+  render() {
+ 
+    return(
+      <div className="blue card">
+        <p>Reg form placeholder</p>
+        <p>Submit</p>
+      </div>
+    )
+  }
+}
+
+export default RegForm;
